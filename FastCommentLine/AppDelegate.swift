19e09821a14a6975ce62940e46dcea12b9db82b3
Eric Baker <eric@C02P112KG3QD.local>
2016-09-28 11:00:31 -0500

    Initial commit

---
+++ FastCommentLine/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  FastCommentLine
+//
+//  Created by Eric Baker on 28Sep2016.
+//  Copyright © 2016 DuneParkSoftware, LLC. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    @IBOutlet weak var window: NSWindow!
+}
---
+++ FastCommenter/SourceEditorCommand.swift
@@ -0,0 +1,54 @@
+//
+//  SourceEditorCommand.swift
+//  FastCommenter
+//
+//  Created by Eric Baker on 28Sep2016.
+//  Copyright © 2016 DuneParkSoftware, LLC. All rights reserved.
+//
+
+import Foundation
+import XcodeKit
+
+class SourceEditorCommand: NSObject, XCSourceEditorCommand {
+    
+    func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) -> Void {
+        var newSelections = [XCSourceTextRange]()
+        var updatedLines = [Int]()
+
+        invocation.buffer.selections.forEach { selection in
+            if let range = selection as? XCSourceTextRange {
+                (range.start.line...range.end.line).forEach { lineIndex in
+                    guard lineIndex < invocation.buffer.lines.count else {
+                        completionHandler(nil)
+                        return
+                    }
+                    
+                    if let line = invocation.buffer.lines[lineIndex] as? String {
+                        if !line.hasPrefix("//") {
+                            invocation.buffer.lines[lineIndex] = "// \(line)"
+                            updatedLines.append(lineIndex)
+                        }
+                        else {
+                            let offset = line.hasPrefix("// ") ? 3 : 2
+                            let uncommentedLine = line.substring(from: line.index(line.startIndex, offsetBy: offset))
+                            invocation.buffer.lines[lineIndex] = uncommentedLine
+                            updatedLines.append(lineIndex)
+                        }
+                    }
+                }
+
+                let textRange = XCSourceTextRange()
+                let newPosition = XCSourceTextPosition(line: range.end.line + 1, column: 0)
+                textRange.start = newPosition
+                textRange.end = newPosition
+
+                newSelections.append(textRange)
+            }
+        }
+
+        invocation.buffer.selections.setArray(newSelections)
+
+        completionHandler(nil)
+    }
+    
+}
