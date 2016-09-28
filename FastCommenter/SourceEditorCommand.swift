3badb938dcc8d8c7b7ed13110ae5ae8261413eaf
Eric Baker <eric@C02P112KG3QD.local>
2016-09-28 16:26:25 -0500

    Use defer

--- FastCommenter/SourceEditorCommand.swift
+++ FastCommenter/SourceEditorCommand.swift
@@ -12,16 +12,17 @@ import XcodeKit
 class SourceEditorCommand: NSObject, XCSourceEditorCommand {
     
     func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) -> Void {
+        defer {
+            completionHandler(nil)
+        }
+
         var newSelections = [XCSourceTextRange]()
         var updatedLines = [Int]()
 
         invocation.buffer.selections.forEach { selection in
             if let range = selection as? XCSourceTextRange {
                 (range.start.line...range.end.line).forEach { lineIndex in
-                    guard lineIndex < invocation.buffer.lines.count else {
-                        completionHandler(nil)
-                        return
-                    }
+                    guard lineIndex < invocation.buffer.lines.count else { return }
                     
                     if let line = invocation.buffer.lines[lineIndex] as? String {
                         if !line.hasPrefix("//") {
@@ -47,8 +48,6 @@ class SourceEditorCommand: NSObject, XCSourceEditorCommand {
         }
 
         invocation.buffer.selections.setArray(newSelections)
-
-        completionHandler(nil)
     }
     
 }
